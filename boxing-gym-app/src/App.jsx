1efa2f6d462d303434c2ec1092de5bed995def26
Prichard Dube <genarodube8@gmail.com>
2025-08-21 22:44:34 +0200

    imported login,signUp and Dashboard components to App.jsx

--- boxing-gym-app/src/App.jsx
+++ boxing-gym-app/src/App.jsx
@@ -1,18 +1,28 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Hero from "./pages/Hero";
+import Navbar from "./components/Navbar";
+import Dashboard from "./pages/Dashboard";
 import Home from "./pages/Home";
-import Navbar from "./pages/Navbar";
+import Login from "./pages/Login";
+import SignUp from "./SignUp";
+
+
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-	      <Route path="/" element={<Navbar/>}/>
-
-      </Routes>
+      <Navbar />
+      <main className="max-w-7xl mx-auto px-6 py-8">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Dashboard/>}/>
+          <Route path="/" element={<Login/>}/>
+          <Route path="/" element={<Hero />}/>
+          <Route path="/" element={<SignUp/>}/>
+        </Routes>
+      </main>
     </Router>
   );
 }
 
 export default App;
-
