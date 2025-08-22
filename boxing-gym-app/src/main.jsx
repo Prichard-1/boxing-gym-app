b14286a99381fac8b57d5f8e48b2ea9ac77fa1b4
Prichard Dube <genarodube8@gmail.com>
2025-08-22 20:50:57 +0200

    Wrap App with BrowserRouter and Auth0ProviderWithHistory for routing and authentication

--- boxing-gym-app/src/main.jsx
+++ boxing-gym-app/src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App.jsx";
+import Auth0ProviderWithHistory from "./auth/Auth0ProviderWithHistory.jsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <Auth0ProviderWithHistory>
+      <App />
+    </Auth0ProviderWithHistory>
+  </BrowserRouter>
+);
+
