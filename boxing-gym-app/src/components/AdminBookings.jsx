7fef52344d5bb9c3d0aed19e1d64afdcbf6c2224
Prichard Dube <genarodube8@gmail.com>
2025-09-05 15:22:32 +0200

    Add useBookings hook

--- boxing-gym-app/src/components/AdminBookings.jsx
+++ boxing-gym-app/src/components/AdminBookings.jsx
@@ -1,4 +1,4 @@
-import useAdminBookings from '../hooks/useAdminBookings';
+import useAdminBookings from './hooks/useAdminBookings';
 
 export default function AdminBookings() {
   const { bookings, loading, error } = useAdminBookings();
