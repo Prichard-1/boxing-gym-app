e660a273e0cce4427a89217d1a1204610b9f8005
Prichard Dube <genarodube8@gmail.com>
2025-09-05 14:25:36 +0200

    fixed config =.js

--- boxing-gym-app/src/config.js
+++ boxing-gym-app/src/config.js
@@ -1,7 +1,7 @@
-// src/config.js
 const config = {
-  API_BASE_URL: import.meta.env.VITE_API_URL || "http://localhost:5000",
+  API_BASE_URL: import.meta.env.VITE_API_BASE_URL?.replace(/\/$/, ''),
 };
 
 export default config;
+
 
