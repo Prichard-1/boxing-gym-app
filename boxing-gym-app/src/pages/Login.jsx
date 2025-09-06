2f4050771c15474406e86092f8f8b5ad293742a4
Prichard Dube <genarodube8@gmail.com>
2025-09-06 13:43:39 +0200

    added a fixed contact file

--- boxing-gym-app/src/pages/Login.jsx
+++ boxing-gym-app/src/pages/Login.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import { toast } from "react-hot-toast";
-import config from "../config";
+import config from "../../config";
 
 export default function Login({ setUser }) {
   const [email, setEmail] = useState("");
